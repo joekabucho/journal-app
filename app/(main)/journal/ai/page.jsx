f106dc701617a6bba498b4c04a0eb77654776f19
joe kabucho <joekabucho2@gmail.com>
2025-03-25 23:46:06 +0300

    ai page refactor 1

--- app/(main)/journal/ai/page.jsx
+++ app/(main)/journal/ai/page.jsx
@@ -1,64 +1,64 @@
-// 'use client'
+'use client'
 
-// import EntryCard from '../../../../components/EntryCard'
-// import NewEntryCard from '../../../../Components/NewEntryCard'
-// import Question from '../../../../Components/Questions'
-// import { getEntries, ask, deleteEntry } from '@/utils/api'
-// import { useEffect, useState } from 'react'
-// import { ReactTyped } from 'react-typed'
+//import EntryCard from '../../../../components/EntryCard'
+//import NewEntryCard from '../../../../Components/NewEntryCard'
+//import Question from '../../../../Components/Questions'
+import { getEntries, ask, deleteEntry } from '@/utils/api'
+import { useEffect, useState } from 'react'
+import { ReactTyped } from 'react-typed'
 
-// const JournalPage = () => {
-//   const [entries, setEntries] = useState([])
-//   const [answer, setAnswer] = useState('')
-//   const [loadingQuestion, setLoadingQuestion] = useState(false)
-//   const [loadingEntries, setLoadingEntries] = useState(false)
+const JournalPage = () => {
+  const [entries, setEntries] = useState([])
+  const [answer, setAnswer] = useState('')
+  const [loadingQuestion, setLoadingQuestion] = useState(false)
+  const [loadingEntries, setLoadingEntries] = useState(false)
 
-//   useEffect(() => {
-//     setLoadingEntries(true)
-//     getEntries().then((data) => {
-//       setEntries(data)
-//       setLoadingEntries(false)
-//     })
-//   }, [])
+  useEffect(() => {
+    setLoadingEntries(true)
+    getEntries().then((data) => {
+      setEntries(data)
+      setLoadingEntries(false)
+    })
+  }, [])
 
-//   const askQuestion = (content) => {
-//     setLoadingQuestion(true)
-//     ask(content).then((response) => {
-//       const data = response.ai ? response.ai : response.error
-//       setAnswer(data)
-//       setLoadingQuestion(false)
-//     })
-//   }
+  const askQuestion = (content) => {
+    setLoadingQuestion(true)
+    ask(content).then((response) => {
+      const data = response.ai ? response.ai : response.error
+      setAnswer(data)
+      setLoadingQuestion(false)
+    })
+  }
 
-//   const removeEntry = (id) => {
-//     deleteEntry(id).then(() => {
-//       getEntries().then((entryData) => {
-//         setEntries(entryData)
-//       })
-//     })
-//   }
+  const removeEntry = (id) => {
+    deleteEntry(id).then(() => {
+      getEntries().then((entryData) => {
+        setEntries(entryData)
+      })
+    })
+  }
 
-//   const renderEntries = (data = []) => {
-//     return data.map((entry) => (
-//       <EntryCard key={entry.id} entry={entry} removeEntry={removeEntry} />
-//     ))
-//   }
+  const renderEntries = (data = []) => {
+    return data.map((entry) => (
+      // <EntryCard key={entry.id} entry={entry} removeEntry={removeEntry} />
+    ))
+  }
 
-//   return (
-//     <div className="p-10 bg-zinc-400/10 overflow-y-scroll">
-//       <div className="flex flex-col">
-//         <Question askQuestion={askQuestion} loading={loadingQuestion} />
-//         <div className="mb-10 flex justify-center items-center whitespace-pre">
-//           <ReactTyped strings={[answer]} typeSpeed={40} showCursor={false} />
-//         </div>
-//       </div>
-//       <div className="grid gap-4 pl-10 mb-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-//         <NewEntryCard />
-//         {renderEntries(entries)}
-//       </div>
-//       {loadingEntries && <div className="text-xl p-10">Loading entries...</div>}
-//     </div>
-//   )
-// }
+  return (
+    <div className="p-10 bg-zinc-400/10 overflow-y-scroll">
+      <div className="flex flex-col">
+        <Question askQuestion={askQuestion} loading={loadingQuestion} />
+        <div className="mb-10 flex justify-center items-center whitespace-pre">
+          <ReactTyped strings={[answer]} typeSpeed={40} showCursor={false} />
+        </div>
+      </div>
+      <div className="grid gap-4 pl-10 mb-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {/* <NewEntryCard /> */}
+        {renderEntries(entries)}
+      </div>
+      {loadingEntries && <div className="text-xl p-10">Loading entries...</div>}
+    </div>
+  )
+}
 
-// export default JournalPage
+export default JournalPage
